9239983fa9656ba9ffd3515dc14ec84a6768fff1
K8 Sosnowska <sosnowska.kk@gmail.com>
2022-01-23 12:04:49 +0100

    Added Internationalization Toolkit plugin files along with a test project

---
+++ InterLocToolkit/Source/InterLocToolkit/InterLocToolkit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InterLocToolkit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InterLocToolkit, "InterLocToolkit" );
---
+++ InterLocToolkit/Source/InterLocToolkit/InterLocToolkitGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InterLocToolkitGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERLOCTOOLKIT_API AInterLocToolkitGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
